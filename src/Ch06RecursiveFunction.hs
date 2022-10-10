522abc8a492a0c8c55074d1dc503836bc7f30026
leechanwoo <cjswosa2@gmail.com>
2022-10-10 13:11:27 +0000

    indexedPeople added

--- src/Ch06RecursiveFunction.hs
+++ src/Ch06RecursiveFunction.hs
@@ -5,6 +5,7 @@ module Ch06RecursiveFunction where
 data Person = Person { age :: Int, name :: String }
   deriving Show
 
-
+indexedPeople :: [(Int, Person)]
+indexedPeople = [(0, haskell), (1, turing), (2, dummy)]
 
 

94301b275f2e2b7cda9967447d494a7645d08e1c
leechanwoo <cjswosa2@gmail.com>
2022-10-10 12:17:00 +0000

    remove test code

--- src/Ch06RecursiveFunction.hs
+++ src/Ch06RecursiveFunction.hs
@@ -2,20 +2,9 @@
 module Ch06RecursiveFunction where
 
 
--- pre script >>>
-
 data Person = Person { age :: Int, name :: String }
   deriving Show
 
---- <<<
-
-
-extractAges :: [(Int, Person)] -> [Int]
-extractAges [] = []
-extractAges (p:ppl) = let (i, p') = p in
-                      let pAge = age p' in 
-                      if pAge > 0 then pAge : extractAges ppl
-                                  else extractAges ppl
 
 
 

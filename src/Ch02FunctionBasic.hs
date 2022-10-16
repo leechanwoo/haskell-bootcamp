1a032cda22b01c56dc21f14fe0252d46fcbee9a1
leechanwoo <cjswosa2@gmail.com>
2022-10-16 03:23:10 +0000

    pre scriptions

--- src/Ch02FunctionBasic.hs
+++ src/Ch02FunctionBasic.hs
@@ -1,3 +1,8 @@
 
 module Ch02FunctionBasic where
 
+data Person = Person { age :: Int, name :: String }
+  deriving Show
+
+p :: Person
+p = Person { age = 23, name = "chris" } 
--- src/Ch07Polymorphism.hs
+++ src/Ch07Polymorphism.hs
@@ -2,3 +2,7 @@
 module Ch07Polymorphism where
 
 
+import Data.List (sort)
+
+
+
